d4b36f298cd433873152879842d24e9e97318327
Gamius00 <GescheaftlichGamius@gmx.de>
2026-09-06 11:36:40 +0200

    fix: remove web purchase redemption from mobile app

--- src/lib/native-intent-redirect.ts
+++
@@ -1,9 +0,0 @@
-import { captureRevenueCatRedemptionUrl } from "./revenuecat-redemption";
-
-export const redirectNativeIntentPath = (path: string) => {
-	try {
-		return captureRevenueCatRedemptionUrl(path) ? "/" : path;
-	} catch {
-		return "/";
-	}
-};
